6894df0561939073262e66ce8387966e4866eeb6
Franco Pereyra <francopereyra325@gmail.com>
2024-07-02 16:04:23 -0300

    agregamos un ejemplo de ventanas emergentes

---
+++ Js/01-variables.js
@@ -0,0 +1,27 @@
+// Esto es un comentario de una linea
+
+
+/* Esto es un comentario
+    de multiples lineas
+*/
+
+// declarar una variable o crear una variable (var - let - const)
+
+let edad = 30;
+let encendido = true;
+
+// mostrar el contenido de una variable
+console.log(edad);
+document.write("<p class='textoResaltado'>Hola mundo</p>");
+document.write("Edad: " + edad + "<br>");
+
+// modificar el valor de una variable
+
+edad = 30 + 5;
+document.write("Edad: " + edad);   
+
+// mostrar en ventanas emergentes
+alert ("Bienvenidos");
+
+const edadUsuario = prompt("Ingrese su edad");
+console.log(edadUsuario);
